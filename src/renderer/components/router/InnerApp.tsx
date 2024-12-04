9c5872f00e58b522f7ae8a6121b692ec31577fd7
Jacek <jacek@clerk.dev>
2024-12-04 17:38:50 -0600

    different session <-> signed-in scenarios

--- src/renderer/components/router/InnerApp.tsx
+++ src/renderer/components/router/InnerApp.tsx
@@ -1,13 +1,21 @@
-import { useAuth, useClerk } from '@clerk/clerk-react'
-import { RouterProvider } from '@tanstack/react-router'
-import { router } from '@/src/app/router'
-import { queryClient } from '@/src/network/QueryClient'
+import { useAuth, useClerk, useSession } from "@clerk/clerk-react";
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "@/src/app/router";
+import { queryClient } from "@/src/network/QueryClient";
 
 export const InnerApp = () => {
-	const clerk = useClerk()
-  const { isSignedIn } = useAuth()
-  console.log("SIGNIN")
-  console.log(isSignedIn)
-  console.log(clerk.session)
-	return <RouterProvider router={router} context={{ clerk, queryClient }} />
-}
+  const clerk = useClerk();
+  const session = useSession();
+  const { isSignedIn } = useAuth();
+  console.log("SIGNIN");
+
+  console.log("-- isSignedIn --");
+  console.log("useAuth().isSignedIn", isSignedIn);
+  console.log("useSession().isSignedIn", session.isSignedIn);
+
+  console.log("-- session --");
+  console.log("useClerk().session", clerk.session);
+  console.log("useSession().session", session.session);
+
+  return <RouterProvider router={router} context={{ clerk, queryClient }} />;
+};
